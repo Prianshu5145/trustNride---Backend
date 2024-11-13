56f407331e1f1aecb28b9d2f938d6cffc223e306
Prianshu5145 <agraharipriyanshu52@gmail.com>
2024-11-14 02:15:03 +0530

    signature add

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -13,12 +13,25 @@ const sendEmail = async (options) => {
     },
   });
 
-  // Email options
+  // Email options with a detailed HTML signature
   const mailOptions = {
-    from: "TRUSTNRIDE <team@trustnride.in>",  // Sender address
-    to: options.email,  // Recipient(s)
-    subject: options.subject,  // Subject line
-    text: options.message,  // Plain text body
+    from: "TRUSTNRIDE <team@trustnride.in>",
+    to: options.email,
+    subject: options.subject,
+    html: `
+      <p>${options.message}</p>
+      <br>
+      <p>----------------------------------------------------</p>
+      <p style="font-size: 1.8em; font-weight: bold;">TRUST N RIDE</p> <!-- Larger size for TRUST N RIDE -->
+      <p style="font-size: 1em; color: #0078D7;">TICK TOCK SOLD!!</p> <!-- Smaller size for TICK TOCK SOLD!! -->
+      <img src="https://res.cloudinary.com/dztz5ltuq/image/upload/v1730227384/IMG-20241023-WA0010_p7ukjb.jpg" alt="TRUST N RIDE Logo" style="width:100px; height:auto; display:block; margin-top:10px;">
+      <p><strong>Phone:</strong> 9792983625</p>
+      <p>Gata Num- 57, near RING ROAD, near NEW JAIHERO, Akbarpur, Ratanpur, Uttar Pradesh 224122</p>
+      <p>Akbarpur, Uttar Pradesh 228161</p>
+      <p><strong>Website:</strong> <a href="https://www.trustnride.in/" target="_blank">www.trustnride.in</a></p>
+      <br>
+      <p><em>This email and its contents are confidential.</em></p>
+    `,
   };
 
   // Send email
