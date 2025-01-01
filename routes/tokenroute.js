41654569f3572e916198979d60ff2496297249bd
Prianshu5145 <agraharipriyanshu52@gmail.com>
2025-01-02 00:48:28 +0530

    increase upload size

--- routes/tokenroute.js
+++ routes/tokenroute.js
@@ -29,7 +29,7 @@ const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024, // Limit file size to 5MB
+    fileSize: 10 * 1024 * 1024, // Limit file size to 5MB
   },
 });
 
