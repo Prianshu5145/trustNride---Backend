c54b2fb6568c286013932db9bd3cb1f2ae1fa5ac
Priyanshu5145 <agraharipriyanshu53@gmail.com>
2025-03-02 17:09:10 +0530

    add smtp server

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,27 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config(); // Load environment variables from .env
+const nodemailer = require("nodemailer");
+require("dotenv").config(); // Load environment variables from .env
 
 const sendEmail = async (options) => {
-  // Create a transporter object using GoDaddy SMTP
+  // Create a transporter object using Brevo SMTP
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // GoDaddy SMTP server
+    host: "smtp-relay.brevo.com", 
+    port: 587, 
+    secure: false, 
     auth: {
-      user: process.env.EMAIL_USER, // Email from .env (your GoDaddy email)
-      pass: process.env.EMAIL_PASS, // Email password from .env
+      user: process.env.EMAIL_USER, 
+      pass: process.env.EMAIL_PASS, 
     },
   });
 
   // Email options with a detailed HTML signature
   const mailOptions = {
-    from: "agraharipriyanshu53@gmail.com",
+    from: `"TRUST N RIDE" <team@trustnride.in>`, // Your email with business name
     to: options.email,
     subject: options.subject,
     html: `
       <p>${options.message}</p>
       <br>
-      <p>----------------------------------------------------</p>
+      <p>------------------------------------------------------------------</p>
       <p style="font-size: 1.8em; font-weight: bold;">TRUST N RIDE</p> <!-- Larger size for TRUST N RIDE -->
       <p style="font-size: 1em; color: #0078D7;">TICK TOCK SOLD!!</p> <!-- Smaller size for TICK TOCK SOLD!! -->
       <img src="https://res.cloudinary.com/dztz5ltuq/image/upload/v1730227384/IMG-20241023-WA0010_p7ukjb.jpg" alt="TRUST N RIDE Logo" style="width:100px; height:auto; display:block; margin-top:10px;">
